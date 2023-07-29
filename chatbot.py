1dd1cbf8fb2fa6178463d9e0a5204067d34ef0c6
asif851 <asifmredha21@gmail.com>
2023-07-30 02:23:07 +0600

    file added

---
+++ chatbot.py
@@ -0,0 +1,100 @@
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+
+words=[]
+classes = []
+documents = []
+ignore_words = ['?', '!']
+data_file = open('intents.json', encoding='utf-8').read()
+intents = json.loads(data_file)
+
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+
+        documents.append((w, intent['tag']))
+
+
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+classes = sorted(list(set(classes)))
+
+print (len(documents), "documents")
+
+print (len(classes), "classes", classes)
+
+print (len(words), "unique lemmatized words", words)
+
+
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
+
+# initializing training data
+training = []
+output_empty = [0] * len(classes)
+for doc in documents:
+    bag = []
+    pattern_words = doc[0]
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append((bag, output_row))  # Store as a tuple instead of a list
+
+random.shuffle(training)
+
+# Convert training data to a NumPy array
+train_x = np.array([i[0] for i in training])  # Extract the bag-of-words representation
+train_y = np.array([i[1] for i in training])  # Extract the output row
+print("Training data created")
+
+
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# equal to number of intents to predict output intent with softmax
+from tensorflow.keras.optimizers import SGD
+
+# ... (rest of your code) ...
+
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons, and 3rd output layer contains
+# number of neurons equal to the number of intents to predict output intent with softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model. Use the latest Keras optimizer without the "decay" argument.
+# The learning rate can be set directly using the "learning_rate" argument.
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# ... (rest of your code) ...
+
+#fitting and saving the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+print("model created")
