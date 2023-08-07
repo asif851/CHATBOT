e051a7268a6adc841c824a9a35f5c9a9b90fd6c9
asif851 <asifmredha21@gmail.com>
2023-08-07 15:47:59 +0600

    added files

--- chatbot.py
+++ chatbot.py
@@ -91,7 +91,6 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-# ... (rest of your code) ...
 
 #fitting and saving the model
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
